853ec15f5d4a64291d726c466ce02fa027b9a683
Azathothas <AjamX101@gmail.com>
2024-10-14 16:42:10 +0545

    +

--- .github/scripts/setup_appbundles_archlinux.sh
+++ .github/scripts/setup_appbundles_archlinux.sh
@@ -16,12 +16,12 @@ setup_appbundles_alpine()
 {
  #https://github.com/xplshn/pelf/blob/pelf-ng/assets/AppRun.rootfs-based
   export APPRUN_URL="https://raw.githubusercontent.com/xplshn/pelf/refs/heads/pelf-ng/assets/AppRun.rootfs-based.stable"
-   if [ -f "/opt/ROOTFS/archlinux.ROOTFS.tar.gz" ] && [ $(du -s "/opt/ROOTFS/archlinux.ROOTFS.tar.gz" | cut -f1) -gt 100 ]; then
+   if [ -f "/opt/ROOTFS/archlinux.ROOTFS.tar.zst" ] && [ $(du -s "/opt/ROOTFS/archlinux.ROOTFS.tar.zst" | cut -f1) -gt 100 ]; then
    #Extract
      pushd "$(mktemp -d)" >/dev/null 2>&1
-     bsdtar -x -f "/opt/ROOTFS/archlinux.ROOTFS.tar.gz" -p -C "./" 2>/dev/null
+     bsdtar -x -f "/opt/ROOTFS/archlinux.ROOTFS.tar.zst" -p -C "./" 2>/dev/null
      ROOTFS_TMPEXT="$(find . -maxdepth 1 -type d -exec basename {} \; | grep -Ev '^\.$' | xargs -I {} realpath {})" && export ROOTFS_TMPEXT="${ROOTFS_TMPEXT}" ; mkdir -p "${ROOTFS_DIR}"
-     [ -n "${ROOTFS_TMPEXT+x}" ] && [[ "${ROOTFS_TMPEXT}" == "/tmp"* ]] && rsync -achLq --delete "${ROOTFS_TMPEXT}/." "${ROOTFS_DIR}" ; popd >/dev/null 2>&1
+     [ -n "${ROOTFS_TMPEXT+x}" ] && [[ "${ROOTFS_TMPEXT}" == "/tmp"* ]] && rsync -achq --delete "${ROOTFS_TMPEXT}/." "${ROOTFS_DIR}" ; popd >/dev/null 2>&1
      if [ -d "${ROOTFS_DIR}" ] && [ $(du -s "${ROOTFS_DIR}" | cut -f1) -gt 100 ]; then
        realpath "${ROOTFS_DIR}" && ls "${ROOTFS_DIR}" -lah && du -sh "${ROOTFS_DIR}"
      else
@@ -39,27 +39,39 @@ setup_appbundles_alpine()
      ln --symbolic --force "../../bin/bash" "${ROOTFS_DIR}/usr/bin/bash"
     ##DO NOT DO THIS 
      #sudo chmod u+s "${APPDIR}/usr/bin/bwrap"
-   #Fix ID
+    #Fix ID
      unlink "${ROOTFS_DIR}/var/lib/dbus/machine-id" 2>/dev/null
      rm -rvf "${ROOTFS_DIR}/etc/machine-id"
-     "${APPDIR}/AppRun" --Xbwrap --uid "0" --gid "0" -- systemd-machine-id-setup
+     "${APPDIR}/AppRun" --uid "0" --gid "0" systemd-machine-id-setup --print 2>/dev/null | tee "${ROOTFS_DIR}/var/lib/dbus/machine-id"
      ln --symbolic --force --relative "${ROOTFS_DIR}/var/lib/dbus/machine-id" "${ROOTFS_DIR}/etc/machine-id"
-     touch "${ROOTFS_DIR}/var/lib/dbus/machine-id"
-   #Fix NameServers
+    #Fix NameServers
+     unlink "${ROOTFS_DIR}/etc/resolv.conf" 2>/dev/null
      echo -e "nameserver 8.8.8.8\nnameserver 2620:0:ccc::2" > "${ROOTFS_DIR}/etc/resolv.conf"
      echo -e "nameserver 1.1.1.1\nnameserver 2606:4700:4700::1111" >> "${ROOTFS_DIR}/etc/resolv.conf"
-     unlink "${ROOTFS_DIR}/etc/resolv.conf" 2>/dev/null
-   #Fix locale
+    #Fix locale
      echo "LANG=en_US.UTF-8" > "${ROOTFS_DIR}/etc/locale.conf"
      echo "LANG=en_US.UTF-8" >> "${ROOTFS_DIR}/etc/locale.conf"
      echo "LANGUAGE=en_US:en" >> "${ROOTFS_DIR}/etc/locale.conf"
      echo "LC_ALL=en_US.UTF-8" >> "${ROOTFS_DIR}/etc/locale.conf"
      echo "en_US.UTF-8 UTF-8" >> "${ROOTFS_DIR}/etc/locale.gen"
      echo "LC_ALL=en_US.UTF-8" >> "${ROOTFS_DIR}/etc/environment"
-     "${APPDIR}/AppRun" --Xbwrap --uid "0" --gid "0" -- locale-gen
-     "${APPDIR}/AppRun" --Xbwrap --uid "0" --gid "0" -- locale-gen "en_US.UTF-8"
-   #Fix os-release
+     "${APPDIR}/AppRun" --uid "0" --gid "0" locale-gen
+     "${APPDIR}/AppRun" --uid "0" --gid "0" locale-gen "en_US.UTF-8"
+    #Fix os-release
      ln --symbolic --force --relative "${ROOTFS_DIR}/usr/lib/os-release" "${ROOTFS_DIR}/etc/os-release"
+    #SysUpdate
+     rm -rvf "${ROOTFS_DIR}/var/lib/pacman/sync/"*
+     rm -rvf "${ROOTFS_DIR}/etc/pacman.d/gnupg/"*
+     sed '/DownloadUser/d' -i "${ROOTFS_DIR}/etc/pacman.conf"
+     sed 's/^.*Architecture\s*=.*$/Architecture = auto/' -i "${ROOTFS_DIR}/etc/pacman.conf"
+     sed 's/^.*SigLevel\s*=.*$/SigLevel = Never/' -i "${ROOTFS_DIR}/etc/pacman.conf"
+     #echo "Server = https://geo.mirror.pkgbuild.com/\$repo/os/\$arch" >> "${ROOTFS_DIR}/etc/pacman.d/mirrorlist"
+     curl -qfsSL "https://archlinux.org/mirrorlist/all/https/" | sed '/\(pkgbuild\.com\|rackspace\.com\)/s/^#//' > "${ROOTFS_DIR}/etc/pacman.d/mirrorlist"
+     "${APPDIR}/AppRun" --uid "0" --gid "0" pacman -Syy archlinux-keyring pacutils --noconfirm
+     "${APPDIR}/AppRun" --uid "0" --gid "0" pacman-key --init
+     "${APPDIR}/AppRun" --uid "0" --gid "0" pacman-key --populate "archlinux"
+     "${APPDIR}/AppRun" --uid "0" --gid "0" pacman -y --sync --refresh --refresh --sysupgrade --noconfirm --debug
+     "${APPDIR}/AppRun" --uid "0" --gid "0" pacman -Scc --noconfirm
    #SysUpdate
      rm -rvf "${ROOTFS_DIR}/var/lib/pacman/sync/"*
      rm -rvf "${ROOTFS_DIR}/etc/pacman.d/gnupg/"*
